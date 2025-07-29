6716d55c3c8e4710b420ffa7660cfe0f10fc7e8b
Матвей Черепанов <matvei.s.cherepanov@tusur.ru>
2025-07-29 14:51:36 +0700

    feat(app): интеграция TickersViewModel с QML

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,16 +1,20 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
-
 #include <QLocale>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <QTranslator>
+
+#include "../presentation/viewmodels/tickersviewmodel.h"
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+
     QGuiApplication app(argc, argv);
 
+    // Установка локализации
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
@@ -22,12 +26,23 @@ int main(int argc, char *argv[])
     }
 
     QQmlApplicationEngine engine;
+
+    // ViewModel для QML
+    TickersViewModel tickersModel;
+    engine.rootContext()->setContextProperty("tickersModel", &tickersModel);
+
+    // Загрузка главного QML-файла
     const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && objUrl == url)
                 QCoreApplication::exit(-1);
-        }, Qt::QueuedConnection);
+        },
+        Qt::QueuedConnection);
+
     engine.load(url);
 
     return app.exec();
